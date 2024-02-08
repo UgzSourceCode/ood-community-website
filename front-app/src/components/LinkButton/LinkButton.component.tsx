1709adbe1fddbc4dc63eaabdb897db967b8940bb
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2024-02-08 05:54:48 +0100

    refactored LinkButton.component

--- front-app/src/components/LinkButton/LinkButton.component.tsx
+++ front-app/src/components/LinkButton/LinkButton.component.tsx
@@ -12,7 +12,9 @@ export type LinkButtonProps = {
 export const LinkButton = ({ to, target, rel, className, children }: LinkButtonProps) => {
     return (
         <Link to={to} target={target} rel={rel}>
-            <button className={className} children={children} />
+            <button className={className} >
+                {children}
+            </button>
         </Link>
     );
 };
