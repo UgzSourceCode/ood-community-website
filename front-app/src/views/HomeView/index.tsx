35aadd592896867c0cdd971965ef73b1263aa850
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2024-02-08 06:13:54 +0100

    added place for AboutUs.component on HomeView

--- front-app/src/views/HomeView/index.tsx
+++ front-app/src/views/HomeView/index.tsx
@@ -1,11 +1,13 @@
 import { TopHeader } from "../../components/TopHeader";
 import { Banner } from "../../components/Banner";
+import {AboutUs} from "../../components/AboutUs";
 
 export const HomeView = () => {
     return (
         <main>
             <Banner />
             <TopHeader />
+            <AboutUs />
         </main>
     );
 };
