58f81111c784a7ba8148faf7e6ce1233d0c0e375
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2024-02-08 04:15:09 +0100

    installed i18n with extensions and prepared basic configutarion

--- front-app/src/main.tsx
+++ front-app/src/main.tsx
@@ -1,13 +1,17 @@
 import "./index.css";
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HomeView } from './views/HomeView';
 import { BrowserRouter } from 'react-router-dom';
 
+import "./i18n.ts";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HomeView />
-    </BrowserRouter>
+      <BrowserRouter>
+          <Suspense fallback="loading...">
+              <HomeView />
+          </Suspense>
+      </BrowserRouter>
   </React.StrictMode>,
 )
